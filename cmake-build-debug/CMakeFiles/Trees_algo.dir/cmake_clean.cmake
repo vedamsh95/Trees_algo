dbe50dfd6e1019384e1f9b03e30f43c27610821e
vedamsh <vedamsh95@gmail.com>
2021-06-03 12:53:03 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Trees_algo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trees_algo.dir/main.cpp.obj"
+  "Trees_algo.exe"
+  "Trees_algo.exe.manifest"
+  "Trees_algo.pdb"
+  "libTrees_algo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trees_algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Trees_algo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Trees_algo.
+# This may be replaced when dependencies are built.
